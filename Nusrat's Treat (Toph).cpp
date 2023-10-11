97d63922a3fb35301300a8dd2fee699f3a080e19
Sumaiya Hossain Surovi <ssur.ovi03@gmail.com>
2023-10-11 13:21:45 +0600

    Solution of  Nusrat's Treat (Toph).cpp

---
+++ Nusrat's Treat (Toph).cpp
@@ -0,0 +1,15 @@
+// Problem Link
+// https://toph.co/p/nusrat-apur-treat
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n,count;
+	cin >> n;
+	count = (n*(n+1))/2;
+	cout << count;	
+	return 0;
+}
+
